728315f270cd421a25db19a233372fc44d7a9d05
Tabiris <M.Niepalla@gmx.de>
2021-12-03 17:55:54 +0000

    Basic construct for the database. Grants access onto the struct "PxFeatureVector" which is used as a dummy for later use.

---
+++ src/DB/PxFeatureVector.py
@@ -0,0 +1,16 @@
+
+
+class PxFeatureVector():
+    attributes = { #TODO Must be implemented!!
+        0:"0"
+    }
+
+
+    def __init__(self):
+        pass
+    def getAttribute(self, num):
+        return -1
+    def getAttributes(self):
+        return -1
+    def comparePxVectors(self,compareWith):
+        return -1 #TODO Comparing function for the ranking.
