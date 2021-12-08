1c3088c461bb7327a38029f542af3c416751bc09
Tabiris <M.Niepalla@gmx.de>
2021-12-08 17:24:03 +0100

    Small style changes.

--- src/DB/Person.py
+++ src/DB/Person.py
@@ -1,50 +1,48 @@
-
-
 import collections
 import PxFeatureVector
+
 
 class Person:
     myPxFeatureVectors = collections.deque(maxlen=5)
     id = -1
     age = -1
 
-    def __init__(self,myID):
+    def __init__(self, myID):
         self.id = myID
 
-    def getPxVector(self,num):
+    def getPxVector(self, num):
         return self.myPxFeatureVectors.index(num)
 
     def getPxVectors(self):
         return self.myPxFeatureVectors
 
-    def addPxVector(self,newVector):
-        if(0 == 0):
-            pass #Implement checkup if newVector is a fitting vector!
+    def addPxVector(self, newVector):
+        if 0 == 0:
+            pass  # Implement checkup if newVector is a fitting vector!
         self.myPxFeatureVectors.append(newVector)
         age = 0
         return 1
 
-    def addPxVectors(self,newVectors):
+    def addPxVectors(self, newVectors):
         for newVector in newVectors:
             self.addPxVector(newVector)
 
-    def compareMeToPxVector(self,PxVector):
+    def compareMeToPxVector(self, PxVector):
         averageRanking = 0
         amountOfFrames = 0
 
         for compareWith in self.myPxFeatureVectors:
             averageRanking += compareWith.comparePxVectors(compareWith)
             amountOfFrames += 1
-        if(amountOfFrames == 0):
+        if amountOfFrames == 0:
             return -1
-        return (averageRanking / amountOfFrames)
-        
+        return averageRanking / amountOfFrames
+
     def getAge(self):
         return self.age
 
     def getID(self):
         return self.id
-    
-    def setID(self,myID):
-        self.id = myID
 
+    def setID(self, myID):
+        self.id = myID
--- src/DB/database.py
+++ src/DB/database.py
@@ -1,75 +1,72 @@
 import Person
 
 
-
 class database:
     allPersons = []
-    lastUsedID = 0 #Actually used??
+    lastUsedID = 0  # Actually used??
     threshhold = 2
     personTimeToLive = 5
 
-    def __init__(self,newThreshhold, TTTForPersons):
+    def __init__(self, newThreshhold, TTTForPersons):
         """Creates a database with the given threshholds.
         If none are given the parameters will be set as:
         threshhold for similarity: 0.9
-        TimeToLiveForPersons (in frames): 5 """
-        if(newThreshhold > 0 & newThreshhold < 1):
+        TimeToLiveForPersons (in frames): 5"""
+        if newThreshhold > 0 & newThreshhold < 1:
             self.threshhold = newThreshhold
-        
-        if(TTTForPersons > 0 & TTTForPersons < 9999):
-            self.personTimeToLive = TTTForPersons
-        
 
-    def updatePersonByVector(self,newPxVector):
+        if TTTForPersons > 0 & TTTForPersons < 9999:
+            self.personTimeToLive = TTTForPersons
+
+    def updatePersonByVector(self, newPxVector):
         bestRank = -1
         foundID = -1
 
         for compareWith in self.allPersons:
-            newRank = compareWith.compareMeToPxVector(self,newPxVector)
-            if(newRank > bestRank):
+            newRank = compareWith.compareMeToPxVector(self, newPxVector)
+            if newRank > bestRank:
                 bestRank = newRank
                 foundID = compareWith.getID()
 
-        if(bestRank > self.threshhold):
+        if bestRank > self.threshhold:
             self.allPersons[foundID].addPxVector(newPxVector)
             return foundID
         return -1
 
-
-    def addPerson(self,newPerson):
-        """First checks if this person already exists. 
+    def addPerson(self, newPerson):
+        """First checks if this person already exists.
         If several existing persons match the best match (aka highest ranked)
         will be picked and updated with the vectors from newPerson.
         If no match was found a new person will be created and added.
         """
         oldID = self.__searchPerson(newPerson)
 
-        if(oldID < 0):
+        if oldID < 0:
             nextID = self.__createNewID()
             newPerson.setID(nextID)
-            self.allPersons.insert(nextID,newPerson)
+            self.allPersons.insert(nextID, newPerson)
             oldID = nextID
-        else:           
+        else:
             personToUpdate = self.allPersons[oldID]
-            personToUpdate.addPxVectors(self,newPerson.getPxVectors())
+            personToUpdate.addPxVectors(self, newPerson.getPxVectors())
 
         return oldID
 
-    def deletePersonByVector(self,pxVector):
-        """Checks if a person's vectors are in average similar enough to the given vector 
-           to be declared as another vector of this person.
-           If more exist, the closest (aka highest ranked) will be picked and deleted.
+    def deletePersonByVector(self, pxVector):
+        """Checks if a person's vectors are in average similar enough to the given vector
+        to be declared as another vector of this person.
+        If more exist, the closest (aka highest ranked) will be picked and deleted.
         """
         personToDelete = Person.Person(-1)
         personToDelete.addPxVector(pxVector)
         return self.deletePerson(personToDelete)
 
-    def deletePerson(self,personToDelete):
+    def deletePerson(self, personToDelete):
         """Checks if a person is similar enough to the given person to be declared as the same.
-           If more exist, the closest (aka highest ranked) will be picked and deleted.
+        If more exist, the closest (aka highest ranked) will be picked and deleted.
         """
         id = self.__searchPerson(personToDelete)
-        if(id < 0):
+        if id < 0:
             return id
         else:
             del self.allPersons[id]
@@ -82,27 +79,28 @@ class database:
         nextID = 0
 
         for person in self.allPersons:
-            if(person.getID() == nextID):
+            if person.getID() == nextID:
                 nextID += 1
         return nextID
 
-
     def __searchPerson(self, searchedPerson):
         """Searches for the given person, returns:
-           The person's ID if one close enough to the given exists.
-           -1 Otherwise. 
-           """
+        The person's ID if one close enough to the given exists.
+        -1 Otherwise.
+        """
 
         bestRank = -1
         foundID = -1
 
         for compareWith in self.allPersons:
-            newRank = compareWith.compareMeToPxVector(self,searchedPerson.getPxVectors())
-            if(newRank > bestRank):
+            newRank = compareWith.compareMeToPxVector(
+                self, searchedPerson.getPxVectors()
+            )
+            if newRank > bestRank:
                 bestRank = newRank
                 foundID = compareWith.getID()
 
-        if(bestRank > self.threshhold):
+        if bestRank > self.threshhold:
             return foundID
         return -1
 
@@ -111,5 +109,5 @@ class database:
 
         for checkPerson in self.allPersons:
             age = checkPerson.getAge()
-            if(age > self.personTimeToLive):
+            if age > self.personTimeToLive:
                 del self.allPersons[checkPerson.getID()]
