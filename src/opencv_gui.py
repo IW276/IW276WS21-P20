dfcd0eeb88f99891f7577725e28a7e5ab6e4bf46
Rilakin <rilakin94@gmail.com>
2021-12-11 12:00:25 +0100

    GUI changes
    image rescaling
    toolbar prep

--- src/opencv_gui.py
+++ src/opencv_gui.py
@@ -2,6 +2,7 @@ import time
 
 import cv2
 import ImgIngest
+import numpy
 
 
 def draw_on_image(image_to_edit, top_left_corner, bottom_right_corner, person_id):
@@ -28,28 +29,49 @@ class OpencvGUI:
     window_name = 'Personenidentifikation mit rapid Re-Identification'
 
     def run(self, image_dir_param, detection_file_param):
+        # init ingest
         ingestion = ImgIngest.ImgIngest()
         ingestion.set_path_img_folder(image_dir_param)
         ingestion.set_path_detection_file(detection_file_param)
         ingestion.read_detection_file()
+
         start_time = time.time()
+        scale_percent_1 = 80
+
+        toolbar = numpy.zeros((512, 512, 1), dtype="uint8")
         while True:
             image_info = ingestion.get_frame_info()
-            self.update_image(image_info['img'], self.coordinates)
-            cv2.imshow(self.window_name, self.image)
+
+            # draw BBs
+            self.update_image(image_info['img'], image_info['data'])
+
+            # image scaling
+            width_1 = int(self.image.shape[1] * (scale_percent_1 / 100))
+            height_1 = int(self.image.shape[0] * (scale_percent_1 / 100))
+            dim_1 = (width_1, height_1)
+            rescaled_toolbar = cv2.resize(toolbar, dim_1, interpolation=cv2.INTER_AREA)
+            rescaled_image = cv2.resize(self.image, dim_1, interpolation=cv2.INTER_AREA)
+
+            cv2.imshow(self.window_name, rescaled_image)
             cv2.waitKey(1)
 
             if image_info['frame_count_total'] <= image_info['current_frame']:
                 end_time = time.time()
-                print('fps =' + str(image_info['frame_count_total']/(end_time - start_time)))
+                print('fps =' + str(image_info['frame_count_total'] / (end_time - start_time)))
                 cv2.destroyAllWindows()
                 return
 
-    def update_image(self, image_path, coordinates):
+    def update_image(self, image_path, coords):
         self.image = cv2.imread(image_path.__str__(), cv2.IMREAD_COLOR)
-
-        for entry in self.coordinates:
-            self.image = draw_on_image(self.image, entry['top'], entry['bottom'], entry['id'])
+        placeholder_id = 1
+        for row in coords.itertuples():
+            placeholder_id = placeholder_id + 1
+            self.image = draw_on_image(self.image,
+                                       row[3],
+                                       row[4],
+                                       row[5],
+                                       row[6],
+                                       1)
         return
 
 
