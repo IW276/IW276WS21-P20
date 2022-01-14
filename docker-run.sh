07726131235f0bd07c1ef1b566cff2f1d962fe3e
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2022-01-14 11:33:11 +0100

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,4 @@ xhost +local:docker
 docker run --rm \
           -e DISPLAY \
           -v /tmp/.X11-unix:/tmp/.X11-unix \
-          reid_p20_final \
-          "cd /deep-person-reid && python3 setup.py develop && cd /src"
+          reid_p20_final
